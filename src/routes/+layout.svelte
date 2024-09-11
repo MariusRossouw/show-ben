63cc3955f4ec7f1fe87a271b7b1c2692c62ad378
Marius Rossouw <marius@stratech.co.za>
2024-09-11 08:58:14 -0600

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { FetchInterceptorV2 } from '@stratech-team/svelte-component-library';
+
+	//Import all the styling for the app
+	import '../app.css';
+	// data returned from the +layout.js (ENV data)
+	export let data;
+</script>
+
+<FetchInterceptorV2 {data} />
+
+<slot />
